6ab7f10e162ef0587068fe7fb60189aba465a4ff
Thijs Roem <thijsroem@gmail.com>
2020-01-14 13:55:10 +0100

    post processing menu fix, music fix

---
+++ SeminarOndernemen/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        DontDestroyOnLoad(audioSource);
+    }
+}
